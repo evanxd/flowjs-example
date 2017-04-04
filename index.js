ba7f6d4f5fcf1f547566d7b4544fe112af288811
Evan Tseng <evan@tseng.io>
2017-04-04 09:24:16 +0800

    Use default templates

--- index.js
+++ index.js
@@ -2,16 +2,9 @@ var flow = require('node-flowjs');
 
 flow
 .setup('expenditure-application-workflow', {
-  approverId:           'wowens',
-  requestMailSubject:   'Request An Approval',
-  requestMailContent:   './template/request.html',
-  rejectionMailSubject: 'The Application Is Rejected',
-  rejectionMailContent: './template/rejection.html',
-  approvalMailSubject:  'Got The Approval',
-  approvalMailContent:  './template/approval.html',
+  approverId: 'wowens',
 })
 .mailhook({
-  fromEmail: 'member@your-org.com',
   subject: 'expenditure-application-workflow',
   applicantIdSelector: 'table tr:first-child td:last-child',
 });
